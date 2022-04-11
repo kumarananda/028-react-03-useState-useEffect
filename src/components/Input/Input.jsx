65ca509a7eb933def414e94a722d3af31494e8cc
kumarananda <coderanandakumar@gmail.com>
2022-04-12 04:21:50 +0600

    com delete

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import './Input.css'
 
 const Input = () => {
@@ -91,18 +93,19 @@ export const InputForm = ( ) => {
 
 export const InputForm2 = ( ) => {
 
-
+    // from input with useState
     const [input, setInput ] = useState({
         name : '',
         email : '',
         cell : '',
         uname : '',
-        gender : ''
+        gender : '',
+        photo : ''
     });
     // get value without destracring
     // console.log(input.name+" "+ input.email+" "+ input.cell+" "+ input.uname + " "+ input.gender);
     // with destracring
-    let {name,email,cell, uname, gender } = input;
+    let {name,email,cell, uname, gender, photo } = input;
 
     // alert manage
     const [alert, setalert] = useState({
@@ -113,25 +116,45 @@ export const InputForm2 = ( ) => {
 
     const hundleFornSubmit = (e) => {
         e.preventDefault()
-        if( name === "", email === '', cell === '', uname === '', gender === ''){
+        if( name === "", email === '', cell === '', uname === '', gender === '', photo === ''){
             setalert({
                 msg : "this is alert",
                 type : 'danger',
                 status : true
             })
         }else {
-            setalert({
-                msg : "Submited",
-                type : 'success',
-                status : true
-            })
-            setInput({
-                name : '',
-                email : '',
-                cell : '',
-                uname : '',
-                gender : ''
-            })
+
+            axios.post('http://localhost:5050/users',  input //03  //{name,email,cell,uname,gender,photo} //02
+            // {  
+            //     id : '',
+            //     name : name,
+            //     email : email,
+            //     cell : cell,
+            //     uname : uname,
+            //     gender : gender,
+            //     photo : photo
+            // } //01
+            ).then(res  => {
+                setalert({
+                    msg : "Submited",
+                    type : 'success',
+                    status : true
+                })
+                setInput({
+                    name : '',
+                    email : '',
+                    cell : '',
+                    uname : '',
+                    gender : '',
+                    photo : ''
+                })
+            }
+            ).catch(error => {
+                console.log(error);
+            }
+            )
+
+            
         }
     }
     // Alert Close
@@ -143,11 +166,29 @@ export const InputForm2 = ( ) => {
         })
     }
 
+    // get ALLL USER DATA
+    const [users, setUsers] = useState([])
+    // get ALLL USER DATA
+    useEffect( () => {
+        axios.get('http://localhost:5050/users').then(res => {
+            setUsers(res.data)
+
+        }
+        ).catch(error => {
+            console.log(error);
+        })
+    },[ users]) //will change after edit 
+
+
+    const hendleDataDelete = (id) => {
+        axios.delete('http://localhost:5050/users/'+ id);
+    }
+
     return (
         <>
-            <div className="container firstTest mt-4">
+            <div className="container-fluid firstTest mt-4">
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                         <div className="card p-2 ">
                         <h3 className='text-center'>Input Form 3</h3>
                             <div className="card-body text">
@@ -158,20 +199,24 @@ export const InputForm2 = ( ) => {
 
                                 <form action="" onSubmit={ hundleFornSubmit }>
                                     <div className="my-3">
-                                        <label htmlFor="">Name</label>
+                                        <label htmlFor="">Name</label>: {name}
                                         <input className="form-control" value={ name } onChange={e => setInput(  { ...input, name : e.target.value }  )} type="text"/>
                                     </div>
                                     <div className="my-3"> 
-                                        <label htmlFor="">Email</label>
+                                        <label htmlFor="">Email</label>: { email }
                                         <input className="form-control" value={ email } onChange={e => setInput( { ...input, email : e.target.value })  } type="text"/>
                                     </div>
                                     <div className="my-3">
-                                        <label htmlFor="">Cell</label>
+                                        <label htmlFor="">Cell</label>: { cell } 
                                         <input className="form-control" value={ cell } onChange={e => setInput( { ...input, cell : e.target.value })} type="text"/>
                                     </div>
                                     <div className="my-3">
                                         <label htmlFor="">User Name</label>
                                         <input className="form-control" value={ uname } onChange={e => setInput( { ...input, uname : e.target.value } )} type="text"/>
+                                    </div>
+                                    <div className="my-3">
+                                        <label htmlFor="">Photo</label>
+                                        <input className="form-control" value={ photo } onChange={e => setInput( { ...input, photo : e.target.value } )} type="text"/>
                                     </div>
                                     <p>Gender</p>
                                     <div className="my-3">
@@ -188,19 +233,61 @@ export const InputForm2 = ( ) => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-2">
                         <div className="card">
                             <div className="card-body">
-                                <h5>Name: {name}</h5>
-                                <h5>Email: {email}</h5>
-                                <h5>Cell: {cell}</h5>
-                                <h5>UserName: {uname}</h5>
-                                <h5>Gander: {gender}</h5>
+                                <p>Name: <br /> {name}</p>
+                                <p>Email: <br /> {email}</p>
+                                <p>Cell: <br /> {cell}</p>
+                                <p>UserName: <br /> {uname}</p>
+                                <p>Gander: <br /> {gender }</p>
+                                <p>Photo</p> <br />
+                                <img src={`${photo}`} style={{width: "50px", height: "50px"}}></img>
                             </div>
                         </div>
                         
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-6">
+                        <div className="card">
+                                <div className="card-header">
+
+                                </div>
+                                <div className="card-body">
+                                    <table className='w-100 table table-striped'>
+                                        <thead>
+                                        <tr>
+                                        <th>Sl</th>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Cell</th>
+                                            <th>UserName</th>
+                                            <th>Gander</th>
+                                            <th>Photo</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                            {
+                                                users.map( (data, index) => 
+                                                    <tr>
+                                                        <td>{index = 1}</td>
+                                                        <td>{data.name}</td>
+                                                        <td>{data.email}</td>
+                                                        <td>{data.cell}</td>
+                                                        <td>{data.uname}</td>
+                                                        <td>{data.gender}</td>
+                                                        <td><img style={{width:"40px", height:"40px"}} src={data.photo} alt="" /></td>
+                                                        <td><button onClick={() => hendleDataDelete(data.id)} className='btn btn-danger btn-sm' >Delete</button></td>
+                                                    </tr>
+                                                
+                                                )
+                                            }
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                        </div>
+                        
 
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './Style.css';
 
 
+
 ReactDOM.render( <App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
